4aa0c07b39dc9fbbc47bddd9e6355fb03eb59bad
Nicolas Iderhoff <nicolasiderhoff@googlemail.com>
2017-10-06 10:26:43 +0200

    added vim as a submodule #2

--- setup/linux-vim.sh
+++ setup/linux-vim.sh
@@ -10,19 +10,11 @@ VIMDIR="$HOME/.dotfiles/vim"
 
 cd "$(dirname "$0")"
 
-# check for anaconda
-
 if [ ! -d "$VIMDIR" ]; then
-    git clone $URL $VIMDIR
+    echo "    Pulling all VIM submodules."
     cd $VIMDIR
-    ln -s $VIMDIR/vimrc $HOME/.dotfiles/vimrc
-    ln -s $VIMDIR/vimrc $HOME/.dotfiles/gvimrc
     git submodule init
     git submodule update
-    cd $HOME
-    dotfiles --sync
-else
-    echo "    $VIMDIR already exists."
 fi
 
 echo "   Done."
